91f2e42520b8b28bbbca41b69fbbe09e17afc9f7
adawang699-zc <adawang699@gmail.com>
2026-06-09 15:49:06 +0800

    fix: add missing migration 0010 for Django startup

---
+++ main/migrations/0010_merge_20260528_2216.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0009_agentlock_portmapping_porttestresult'),
+    ]
+
+    operations = [
+    ]
